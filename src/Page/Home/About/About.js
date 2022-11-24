ec65ec022ee4d5ce2d09987e4efd89de0eb34eb1
Mostafizur <mostafizurdh2000@gmail.com>
2022-11-24 22:01:41 +0600

    added about section

--- src/Page/Home/About/About.js
+++ src/Page/Home/About/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './About.css'
 function About() {
   return (
-    <div className='max-w-[1400px] mx-auto'>About</div>
+    <section className='about_bg'>
+      <div className='max-w-[1400px] mx-auto text-center text-white '>
+        <h1>Abou us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum sint ratione, illo excepturi eius maxime velit ullam earum fuga deleniti optio nemo, quis delectus officiis sunt inventore magni at odio? Iste ducimus rerum magnam nisi distinctio laborum quisquam autem velit nulla suscipit commodi, quos soluta fugit, asperiores ipsum? Quibusdam, amet.</p>
+      </div>
+    </section>
+
   )
 }
 
